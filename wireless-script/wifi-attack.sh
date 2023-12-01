c1a922f5da7cf2fb3ad539d58cc608283afc69e3
Akkislinuxstuff <meabhimanyuchauhan@gmail.com>
2023-12-01 22:07:48 +0530

    modified:   wireless-script/wifi-attack.sh

--- wireless-script/wifi-attack.sh
+++ wireless-script/wifi-attack.sh
@@ -1,173 +1,124 @@
 #!/bin/bash
 
-#color var
+# Color variables
 RED='\033[0;31m'
 NC='\033[0m'
 Y='\033[1;33m'
 
-############### main-manue #################
-function main(){
-	if [ -e /sbin/airmon-ng ]
-	then
-		echo $(clear)
-		figlet -c WiFi-Hacking | lolcat
-		echo ""
-		echo -e "${RED}what you want me to do${NC}?"
-		echo ""
-		echo -e "${Y}1.${NC} ${RED}to only create fake ap type${NC} ${Y}fap${NC}" #done
-		echo -e "${Y}2.${NC} ${RED}to capture a handshake file type${NC} ${Y}handshake${NC}"
-		echo -e "${Y}3.${NC} ${RED}to crack a psswd of a hand shake file type${NC} ${Y}.cap${NC}"
-		echo -e "${Y}4.${NC} ${RED}to enable monitor mode in wlan0 type${NC} ${Y}mon${NC}" #done
-		echo -e "${Y}5.${NC} ${RED}to scan for vulnerability of a device type${NC} ${Y}recon${NC}" #done
-		echo -e "${Y}6.${NC} ${RED}to scen for xss vulnerability tyrp${NC} ${Y}xsscan${NC}"
-		echo -e "${Y}2.${NC} ${RED}to start a xss website using Beef${NC} ${Y}StartXSS${NC}" 
-		echo -e "${Y}7.${NC} ${RED}to restore wireless interface type${NC} ${Y}disable_mon${NC}" #done
-    echo -e "${Y}1.${NC} ${RED}to perform a DoS attack type ${NC} ${Y}dos${NC}"
-		echo -e "${Y}8.${NC} ${RED}to go back type${NC} ${Y}0${NC}"
-    echo ""
+# Function to create a fake AP
+function create_fake_ap() {
+  echo $(clear)
+  figlet -c WiFi-Hacking | lolcat
+  echo ""
+  echo -e "${RED}What do you want to do?${NC}"
+  echo ""
+  echo -e "${Y}1.${NC} ${RED}Create Fake AP${NC}"     # done
+  echo -e "${Y}2.${NC} ${RED}Capture Handshake File${NC}"  # done
+  echo -e "${Y}3.${NC} ${RED}Crack Password of Handshake File${NC}"
+  echo -e "${Y}4.${NC} ${RED}Enable Monitor Mode${NC}"     # done
+  echo -e "${Y}5.${NC} ${RED}Scan for Vulnerability of a Device${NC}"  # done
+  echo -e "${Y}6.${NC} ${RED}Scan for XSS Vulnerability${NC}"
+  echo -e "${Y}7.${NC} ${RED}Restore Wireless Interface${NC}"  # done
+  echo -e "${Y}8.${NC} ${RED}Start a Xss Website with Beef${NC}"
+  echo -e "${Y}9.${NC} ${RED}Go Back${NC}"
+  echo ""
 
-else
-		{
-		echo $(apt install airmon-ng)
-		}&> dev/null
-		echo "installing dependencies"
-		sleep 1
-		air-ng
-fi
-
-while read -p "Enter :" wifi; do
-if [ "$wifi" == fap ]
-	then
-	#fake AP start
-		function sfap(){
-		sleep 2
-		{
-		airbase-ng wlan0mon --essid $ap &
-		}&> /dev/null
-		echo "your Fake AP is created successfully "
-		sleep 3
-		sleep 2
-		bash /opt/Easy-Script/wireless-script/wifi-attack.sh
-		}
-
-		#fake AP
-		function fap(){
-		echo $(clear)
-		figlet Fake-AP
-		echo ""
-		read -p "Enter the name For your Fake AP :" ap
-		sleep 1
-		sfap
-		}
-		fap
-	else
-if [ "$wifi" == ".cap" ]
-	then
-		bash /opt/Easy-Script/wireless-script/crack.sh
-	else
-if [ "$wifi" == "mon" ]
-	then
-		function mon(){
-	spinner() {
-	    local i sp n
-	    sp='/-\|'
-	    n=${#sp}
-	    printf ' '
-	    while sleep 0.1; do
-	        printf "%s\b" "${sp:i++%n:1}"
-	    done
-	}
-
-	printf 'Puttng interface into monitor mode '
-	spinner &
-	{
-	airmon-ng check kill
-	airmon-ng start wlan0  # Enable monitor mode
-	sleep 4 #after some hard work sleaping is important
-	}&> /dev/null
-	kill "$!" # kill the spinner
-	printf '\n'
-	bash /opt/Easy-Script/wireless-script/wifi-attack.sh
-	}
-	mon
-else
-	if [ "$wifi" == "recon" ]
-		then
-			#also recon
-	function scan(){
-	bash /opt/Easy-Script/recon/recon.sh $name
-	}
-
-	#recon
-	function recon(){
-		echo $(clear)
-		figlet recon
-		echo ""
-		read -p "Enter your target ip :" name
-		scan
-		}
-		recon
-else
-	if [ "$wifi" == "disable_mon" ]
-		then
-			#disale monitor mode
-	function disable_mon(){
-	spinner() {
-	    local i sp n
-	    sp='/-\|'
-	    n=${#sp}
-	    printf ' '
-	    while sleep 0.1; do
-	        printf "%s\b" "${sp:i++%n:1}"
-	    done
-	}
-
-	printf 'Restoring your freacking wireless interface '
-	spinner &
-	{
-	airmon-ng stop wlan0mon
-	systemctl start NetworkManager # Disable Monitor mode
-	sleep 4 #after some hard work sleaping is important
-	}&> /dev/null
-	kill "$!" # kill the spinner
-	printf '\n'
-	main
-	}
-	disable_mon
-		else
-			if [ "$wifi" == "0" ]
-				then
-					#back func
-				function 0(){
-						exit
-				}
-				0
-			else
-				if [ "$wifi" == "handshake" ]
-					then
-						bash /opt/Easy-Script/wireless-script/captur.sh
-					else
-						if [ "$wifi" == "xsscan" ]
-							then
-								python3 /opt/Easy-Script/wireless-script/xss-scan.py
-              elif [ "$wifi" == "dos" ]
-              then
-                python3 /opt/Easy-Script/wireless-script/DDoS.py
-				elif ["$wifi" == "StartXSS"]
-				then 
-					echo -e "${RED}Comming ${NC}${Y}Soon${NC}"
-							else
-								clear
-								echo -e "${RED}Invalid ${NC}${Y}Command${NC}"
-								sleep 3
-								main
-				   fi		fi
-			  fi
-		 fi
-	fi
-fi
-fi
-fi
-done
+  while read -p "Enter: " wifi; do
+    case $wifi in
+      1 | fap)
+        create_fake_ap
+        ;;
+      2 | handshake)
+        bash /opt/Easy-Script/wireless-script/captur.sh
+        ;;
+      3 | .cap)
+        bash /opt/Easy-Script/wireless-script/crack.sh
+        ;;
+      4 | mon)
+        enable_monitor_mode
+        ;;
+      5 | recon)
+        scan_for_vulnerability
+        ;;
+      6 | xsscan)
+        python3 /opt/Easy-Script/wireless-script/xss-scan.py
+        ;;
+      7 | disable_mon)
+        disable_monitor_mode
+        ;;
+	  8 | StartXSS)
+	  	startxss
+		;; 
+      9 | 0)
+        exit
+        ;;
+      *)
+        clear
+        echo -e "${RED}Invalid Command${NC}"
+        sleep 3
+        create_fake_ap
+        ;;
+    esac
+  done
 }
+
+# Function to enable monitor mode
+function enable_monitor_mode() {
+  spinner "Putting interface into monitor mode" "airmon-ng check kill; airmon-ng start wlan0" 4
+  bash /opt/Easy-Script/wireless-script/wifi-attack.sh
+}
+
+function startxss(){
+	echo "comming soon"
+}
+
+# Function to disable monitor mode
+function disable_monitor_mode() {
+  spinner "Restoring your wireless interface" "airmon-ng stop wlan0mon; systemctl start NetworkManager" 4
+  main
+}
+
+# Function to scan for vulnerability
+function scan_for_vulnerability() {
+  echo $(clear)
+  figlet recon
+  echo ""
+  read -p "Enter your target IP: " name
+  bash /opt/Easy-Script/recon/recon.sh $name
+}
+
+# Function to create a loading spinner
+function spinner() {
+  local message=$1
+  local command=$2
+  local duration=$3
+
+  printf "$message"
+  {
+    $command
+  } &>/dev/null &
+
+  local pid=$!
+  spin='-\|/'
+
+  for i in $(seq 1 $duration); do
+    sleep 0.1
+    printf "\b${spin:i++%4:1}"
+  done
+
+  wait $pid
+  printf "\n"
+}
+
+# Main function
+function main() {
+  if [ -e /sbin/airmon-ng ]; then
+    spinner "Installing dependencies..." "apt install -y airmon-ng" 3
+    air-ng
+  fi
+
+  create_fake_ap
+}
+
+# Start the script
 main
