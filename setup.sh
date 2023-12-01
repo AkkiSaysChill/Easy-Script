43bd09cf6bc08c8d2c066ff62557cf5301cc2fe8
Akkislinuxstuff <meabhimanyuchauhan@gmail.com>
2023-12-01 21:09:58 +0530

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,32 @@ make_opt_es_dir() {
   echo "Fixed. Run the script again."
 }
 
+create_launcher() {
+  echo "Creating Launcher..."
+  chmod +x install/es
+  mv install/es /usr/local/bin/
+  sleep 2
+  echo "Done now you can remove this folder"
+  echo "type es to run the script"
+  echo "this tool is installed in /opt/Easy-Script/ if you want to remove this tool cd to the install dir and run uninstall.sh"
+  sleep 5
+}
+
+uninstall_sh() {
+  echo "Creating uninstall.sh ..."
+  mv install/uninstall.sh /opt/Easy-Script
+  create_launcher
+}
+
+install_dependencies() {
+  log "Installing dependencies."
+  if [ "$(lsb_release -is)" == "Arch" ]; then
+    pacman -S --noconfirm lolcat toilet figlet cowsay
+  else
+    apt-get -y install lolcat toilet figlet cowsay
+  fi
+}
+
 create_loading_bar() {
   local duration=$1
   local progress_char="."
@@ -33,30 +59,19 @@ create_loading_bar() {
   echo
 }
 
-install_dependencies() {
-  log "Installing dependencies."
-  if [ "$(lsb_release -is)" == "Arch" ]; then
-    pacman -S lolcat toilet figlet cowsay
-  else
-    apt install lolcat toilet figlet cowsay
-  fi
-}
-
-uninstall_sh() {
-  echo "creating uninstall.sh ..."
-  mv install/uninstall.sh /opt/Easy-Script
-  create_launcher
-}
-
-
 install() {
   if [ -e /opt/Easy-Script ]; then
-    # ... (similar changes for the existing code)
-    
-    # Adding a loading bar during the installation
+    mv exploits /opt/Easy-Script && mv creds /opt/Easy-Script/ && mv recon /opt/Easy-Script
+    mv wireless-script /opt/Easy-Script
+    mv easy-script.sh /opt/Easy-Script/ && mv tool.sh /opt/Easy-Script/ && mv recon /opt/Easy-Script
+    mkdir -p /opt/Easy-Script/Cap
+
+    echo "Moving files..."
+    create_loading_bar 0.1
+    clear
     echo "Installing Easy-Script..."
     create_loading_bar 0.1
-
+    clear
     install_dependencies
     uninstall_sh
   else
