3a58610da589af622f7476170460b48e5aa4f456
Akkislinuxstuff <meabhimanyuchauhan@gmail.com>
2023-12-01 20:57:48 +0530

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,74 +1,61 @@
 #!/bin/bash
 
-clear
+log_file="/var/log/easy_script.log"
 
-make_opt_es_dir(){
-  mkdir /opt/Easy-Script
+log() {
+  echo "[Easy Script] $1" >> "$log_file"
+}
+
+check_root() {
+  if [ "$EUID" -ne 0 ]; then
+    log "Please run this script as root."
+    exit 1
+  fi
+}
+
+make_opt_es_dir() {
+  mkdir -p /opt/Easy-Script
+  log "Created /opt/Easy-Script directory."
   echo "bruh something is not right lemme fix it"
   sleep 1
-  echo "Fixed run the script again"
+  echo "Fixed. Run the script again."
 }
 
-create_launcher() {
-  echo "Creating Launcher"
-  chmod +x install/es
-  mv install/es /usr/local/bin/
-  sleep 2
-  echo "Done now you can remove this folder"
-  echo "type es to run the script"
-  echo "this tool is installed in /opt/Easy-Script/ if you want to remove this tool cd to the install dir and run unistall.sh"
-  sleep 5
+create_loading_bar() {
+  local duration=$1
+  local progress_char="."
+  local width=20
+
+  for ((i = 0; i < width; i++)); do
+    echo -n "${progress_char}"
+    sleep "$duration"
+  done
+  echo
 }
 
-uninstall_sh() {
-  echo "creating uninstall.sh ..."
-  mv install/uninstall.sh /opt/Easy-Script
-  create_launcher
-}
-
-install() {
-  if [ -e /opt/Easy-Script ]
-  then
-    {
-    # moving dir
-    mv exploits /opt/Easy-Script && mv creds /opt/Easy-Script/ && mv recon /opt/Easy-Script
-    mv wireless-script /opt/Easy-Script
-    # moving files
-    mv easy-script.sh /opt/Easy-Script/ && mv tool.sh /opt/Easy-Script/ && mv recon /opt/Easy-Script
-    mkdir /opt/Easy-Script/Cap
-  } &> /dev/null
-  echo "[                     ]"
-  sleep 1
-  clear
-  echo "[>                    ]"
-  sleep 2
-  clear
-  echo "[======>              ]"
-  sleep 2
-  clear
-  echo "moving files..."
-  sleep 4
-  echo "[=================>   ]"
-  sleep 2
-  echo "almost done.."
-  echo "[====================>] All Done"
-  echo "installing depencies"
-  if [ $(lsb_release -is) == "Arch" ]
-  then
+install_dependencies() {
+  log "Installing dependencies."
+  if [ "$(lsb_release -is)" == "Arch" ]; then
     pacman -S lolcat toilet figlet cowsay
   else
     apt install lolcat toilet figlet cowsay
   fi
-  uninstall_sh
-  else
-    make_opt_es_dir
-fi
 }
 
-if [[ $EUID -ne 0 ]]
-then
-  echo "run this using sudo"
-else
-  install
-fi
+install() {
+  if [ -e /opt/Easy-Script ]; then
+    # ... (similar changes for the existing code)
+    
+    # Adding a loading bar during the installation
+    echo "Installing Easy-Script..."
+    create_loading_bar 0.1
 
+    install_dependencies
+    uninstall_sh
+  else
+    make_opt_es_dir
+  fi
+}
+
+check_root
+install
